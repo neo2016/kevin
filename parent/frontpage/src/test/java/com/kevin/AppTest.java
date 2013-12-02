0f7b573044ba0fd734ac4553d77992db80108627
kxu913 <kxu913@gmail.com>
2013-12-02 21:42:27 +0800

    add test class and method

---
+++ parent/frontpage/src/test/java/com/kevin/AppTest.java
@@ -0,0 +1,14 @@
+package com.kevin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testHelloWorld() {
+		new App().helloWorld();
+	}
+
+}
